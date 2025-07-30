7e65075c32df3de18a0932bb9c279c092c844424
khanh162hedge <trankhanh1622004@gmail.com>
2025-07-30 07:17:58 +0700

    Fix faucet logic and private key handling

--- convert.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const bs58 = require('bs58');
-
-const secretKey = Uint8Array.from(JSON.parse(fs.readFileSync('wallet.json')));
-const base58Key = bs58.default ? bs58.default.encode(secretKey) : bs58.encode(secretKey);
-
-console.log("Base58 PRIVATE_KEY:\n" + base58Key);
